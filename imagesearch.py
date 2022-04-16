f902fea7dc8ca8e41aaaa6c0ff98b0aaec27e8cf
kiwabc123 <75328842+kiwabc123@users.noreply.github.com>
2022-04-16 17:33:05 +0700

    ...

---
+++ imagesearch.py
@@ -0,0 +1,70 @@
+# Import the libraries
+import os
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.models import Model
+from pathlib import Path
+import os
+import numpy as np
+from PIL import Image
+
+img_root_path = "./dataset"
+fe = FeatureExtractor()
+
+# Iterate through images (Change the path based on your image location)
+# print("Begin extraction")
+
+# features = []
+# for img_path in sorted(Path(img_root_path).glob("*.jpg")):
+
+#     # Extract Features
+#     feature = fe.extract(img=Image.open(img_path))
+#     # Save the Numpy array (.npy) on designated paths
+#     feature_path = "feature.npy"
+#     feature_path = Path("./feature") / (img_path.stem + ".npy")
+#     np.save(feature_path, feature)
+#     features.append(feature)
+
+# print("Finish extraction")
+
+
+# Import the libraries
+import matplotlib.pyplot as plt
+
+
+############ Start searching ####################
+# read npy into features
+img_paths = []
+features = []
+for feature_path in Path("./feature").glob("*.npy"):
+    features.append(np.load(feature_path))
+    img_paths.append(Path(img_root_path) / (feature_path.stem + ".jpg"))
+features = np.array(features)
+
+# Insert the image query
+img = Image.open("./query/0a3806db4557ea538743e9cae5b4a035.jpg")
+# Extract its features
+query = fe.extract(img)
+# Calculate the similarity (distance) between images
+dists = np.linalg.norm(features - query, axis=1)
+# Extract 30 images that have lowest distance
+ids = np.argsort(dists)[:30]
+scores = []
+print(len(img_paths))
+for id in ids:
+    if id < len(img_paths):
+        scores.append((dists[id], img_paths[id]))
+
+#scores = [(dists[id], img_paths[id]) for id in ids]
+# Visualize the result
+axes=[]
+fig=plt.figure(figsize=(8,8))
+for a in range(5*6):
+    score = scores[a]
+    axes.append(fig.add_subplot(5, 6, a+1))
+    subplot_title=str(score[0])
+    axes[-1].set_title(subplot_title)
+    plt.axis('off')
+    plt.imshow(Image.open(score[1]))
+fig.tight_layout()
+plt.show()
---
+++ test.py
@@ -0,0 +1,4 @@
+import numpy as np
+animals = ['cat', 'dog', 'monkey']
+for animal in animals:
+    print(animal)
